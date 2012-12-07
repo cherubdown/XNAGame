6551b6ae0caa3146c93afd3076a48f7c8b12a296
Christopher Bales <radagast1986@yahoo.com>
2012-12-07 00:51:47 -0600

    Iteration 1: Basics

---
+++ XNAGame/Edgar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XNAGame
+{
+    class Edgar : GameUnit
+    {
+        public Edgar()
+        {
+        }
+
+        public Edgar(Vector2 initPosition)
+        {
+            position = initPosition;
+        }
+
+        public void Load(ContentManager Content)
+        {
+            spriteTexture = Content.Load<Texture2D>("Edgar-1");
+        }
+
+        public void Draw(SpriteBatch batch)
+        {
+            batch.Draw(spriteTexture, position, Color.White);
+        }
+    }
+}
---
+++ XNAGame/GameUnit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace XNAGame
+{
+    abstract class GameUnit
+    {
+        protected Texture2D spriteTexture;
+        protected Vector2 position;
+        protected float scale = 1f;      // the factor by which to scale the unit
+
+        public void Load(ContentManager Content)
+        {
+            spriteTexture = Content.Load<Texture2D>("i-am-error");
+        }
+        public void Draw(SpriteBatch batch)
+        {
+            batch.Draw(spriteTexture, new Vector2() { X=0, Y=0 }, Color.White); 
+        }
+
+    }
+}
